eb1a6b46685a704d2e345dfc2215b95118934ca6
Chang Yu Yang <changyuyang06@example.com>
2025-10-08 13:33:50 +0800

    added database connection header and sql files

---
+++ src/sql/create_inventory_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE INVENTORY (
+    inventory_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES USER(user_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
---
+++ src/sql/create_user_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE USER (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(100) NOT NULL,
+    user_subscribe BOOLEAN DEFAULT FALSE,
+    user_password VARCHAR(255) NOT NULL,
+    user_email VARCHAR(150) UNIQUE NOT NULL
+);
