0a39664311d43977971df3f00175987ca7ea6c7b
chiwjieren <jerzltaz@gmail.com>
2025-10-14 10:53:02 +0800

    css header

--- src/main/aboutus.php
+++ src/main/aboutus.php
@@ -5,6 +5,9 @@
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <title>About — GoGreenTogether</title>
   <link rel="stylesheet" href="../css/styles.css">
+  <link rel="stylesheet" href="../css/header.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
   <?php include 'header.php'; ?>
