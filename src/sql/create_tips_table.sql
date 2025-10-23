69c423a3484c201014c6b52fc0675465291dea26
Chang Yu Yang <changyuyang06@example.com>
2025-10-23 21:25:53 +0800

    added tips table

---
+++ src/sql/create_tips_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE TIPS (
+    tips_id INT AUTO_INCREMENT PRIMARY KEY,
+    tips_description TEXT NOT NULL,
+    user_id INT NOT NULL,
+    tips_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES USER(user_id) ON DELETE CASCADE
+);
